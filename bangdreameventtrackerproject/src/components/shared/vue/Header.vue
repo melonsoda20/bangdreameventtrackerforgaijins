00fc02e7e19a5dbd96cfbf14e86e5bb97c00de22
KITINDO\BAT <benedictus.teddy@kreasitindo.co.id>
2019-01-24 07:45:39 +0700

    Add header HTML

---
+++ bangdreameventtrackerproject/src/components/shared/vue/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header center-block">
+        <div class="form-control col-xs-6 .col-md-4">
+            <p class="bandori-app bandori-title">Bandori!</p>
+            <p class="bandori-app bandori-subs">Event tracker</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+@import 'D:\\PracticeProjects\\bangdreameventtrackerproject\\src\\components\\shared\\styles\\Header.css';
+</style>
